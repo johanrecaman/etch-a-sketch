8e11f89f5ebabc259d7d93fe51686e42f16eaf07
Johan Stromberg <logzinr6@gmail.com>
2022-07-22 00:05:55 -0300

    First grid created
    The painting structure is 70% done

---
+++ project/script.js
@@ -0,0 +1,23 @@
+const body = document.body;
+
+let grid = document.createElement('div');
+body.append(grid);
+grid.className = 'grid';
+
+for(let i = 0; i < 256; i++){
+    let divBox = document.createElement('div');
+   
+    divBox.classList.add('box');
+    grid.appendChild(divBox);
+}
+
+const paintBox = document.querySelectorAll(".box");
+let nodeArray = Array.from(paintBox);
+
+nodeArray.forEach((box)=>{
+    box.addEventListener('click', ()=>{
+        box.classList.add("painted");
+    })
+})
+
+
