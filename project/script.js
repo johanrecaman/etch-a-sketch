074db47186b87416fad03dd0e087b72fab1c32c2
Johan Stromberg <logzinr6@gmail.com>
2022-07-22 18:54:35 -0300

    main functionalities added
    still missing all of css design (UI)

--- project/script.js
+++ project/script.js
@@ -1,7 +1,14 @@
 const body = document.body;
+const bodyDiv = document.querySelector('.bodyDiv');
+const buttonEraser = document.querySelector('#eraser');
+const buttonColor = document.querySelector('#color');
+const buttonClear = document.querySelector('#clear');
+
+let isColor = true;
+let isEraser = false;
 
 let grid = document.createElement('div');
-body.append(grid);
+bodyDiv.append(grid);
 grid.className = 'grid';
 
 for(let i = 0; i < 256; i++){
@@ -11,19 +18,35 @@ for(let i = 0; i < 256; i++){
     grid.appendChild(divBox);
 }
 
+buttonEraser.addEventListener('click', ()=>{
+    isEraser = true;
+    isColor = false;
+    console.log(isEraser);
+})
+buttonColor.addEventListener('click',()=>{
+    isColor = true;
+    isEraser = false;
+})
+
 const paintBox = document.querySelectorAll(".box");
 let nodeArray = Array.from(paintBox);
 
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false) 
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 nodeArray.forEach((box)=>{
-    box.addEventListener('mousemove', ()=>{
-        if(mouseDown === true){
-            box.classList.add("painted");
-        }
-    })
+        box.addEventListener('mousemove', ()=>{
+            if(mouseDown === true){
+                if(isEraser === false){
+                    box.classList.add("painted");
+                }
+                else{
+                    box.classList.remove("painted");
+                }
+            }
+        }) 
+        buttonClear.addEventListener('click', ()=>{
+            box.classList.remove('painted');
+        })   
 })
-
-
